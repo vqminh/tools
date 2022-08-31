e800ee21a5cf199cead2f5f3487ce9fd2158acfe
Minh Vu <mivu@linkedin.com>
2022-08-31 20:15:16 +0700

    add login with zalo

--- render-template.js
+++ render-template.js
@@ -1,10 +1,10 @@
-const { readFileSync, writeFile } = require("fs");
+const { readFileSync, writeFile, statSync, readdirSync, mkdir } = require("fs");
+const { dirname } = require("path");
 const { Liquid } = require("liquidjs");
 
 console.log("Processing template");
 
 const _getAllFilesFromFolder = (dir) => {
-  const { readdirSync, statSync } = require("fs");
   let results = [];
 
   readdirSync(dir).forEach((file) => {
@@ -22,20 +22,23 @@ const _getAllFilesFromFolder = (dir) => {
 };
 
 const engine = new Liquid();
-const settings = JSON.parse(readFileSync("settings.json"));
+const settings = JSON.parse(readFileSync(__dirname + "/../settings.json"));
 
 const files = _getAllFilesFromFolder(__dirname + "/template");
 files.forEach((file) => {
   engine
     .renderFile(file, settings) // will read and render `views/hello.liquid`
     .then((content) => {
-      const target = file.replace('tools/template/', '')
-      writeFile(target, content, (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("DONE writing", target)
-        }
+      const target = file.replace("tools/template/", "");
+      mkdir(dirname(target), { recursive: true }, (err) => {
+        if (err) throw err;
+        writeFile(target, content, (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log("DONE writing", target);
+          }
+        });
       });
     });
 });
